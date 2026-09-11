6896bfb3c0d86c6fe49a7945b6c5c18a478a3c87
rushi <kavadrushi01@gmail.com>
2026-09-11 09:25:31 +0530

    Fix seed script to use MONGO_URL env var

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const initdata = require('./data');
 const Listing = require('../models/listing');
 
-const MONGO_URL = "mongodb://localhost:27017/wanderlust";
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/wanderlust";
 
 main().then(() => {
     console.log("Connected to MongoDB");
